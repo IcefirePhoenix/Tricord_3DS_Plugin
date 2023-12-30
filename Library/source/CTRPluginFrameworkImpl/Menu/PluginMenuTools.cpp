71087ddea0c29d6e5e4409bb9b6282c05ef00af7
IcefirePhoenix <109490792+IcefirePhoenix@users.noreply.github.com>
2023-12-29 22:25:20 -0800

    Revert disabled Auto-Save/Load settings

--- Library/source/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
+++ Library/source/CTRPluginFrameworkImpl/Menu/PluginMenuTools.cpp
@@ -88,7 +88,7 @@ namespace CTRPluginFramework
         //if (Preferences::IsEnabled(Preferences::UseFloatingBtn)) (*item++)->AsMenuEntryImpl().Enable();
         //else (*item++)->AsMenuEntryImpl().Disable();
 
-        /*if (Preferences::IsEnabled(Preferences::AutoSaveCheats)) (*item++)->AsMenuEntryImpl().Enable();
+        if (Preferences::IsEnabled(Preferences::AutoSaveCheats)) (*item++)->AsMenuEntryImpl().Enable();
         else (*item++)->AsMenuEntryImpl().Disable();
 
         if (Preferences::IsEnabled(Preferences::AutoSaveFavorites)) (*item++)->AsMenuEntryImpl().Enable();
@@ -98,7 +98,7 @@ namespace CTRPluginFramework
         else (*item++)->AsMenuEntryImpl().Disable();
 
         if (Preferences::IsEnabled(Preferences::AutoLoadFavorites)) (*item)->AsMenuEntryImpl().Enable();
-        else (*item)->AsMenuEntryImpl().Disable();*/
+        else (*item)->AsMenuEntryImpl().Disable();
 
         item = _miscellaneousMenu.begin();
 
@@ -579,13 +579,13 @@ namespace CTRPluginFramework
         _settingsMenu.Append(new MenuEntryTools("Set backlight (Experimental)", EditBacklight, false, false));
         //_settingsMenu.Append(new MenuEntryTools("Use touchscreen button to open Tricord", [] { Preferences::Toggle(Preferences::UseFloatingBtn); }, true, Preferences::IsEnabled(Preferences::UseFloatingBtn)));
 
-       /* _settingsMenu.Append(new MenuEntryTools("Auto-save enabled cheats", [] { Preferences::Toggle(Preferences::AutoSaveCheats); }, true, Preferences::IsEnabled(Preferences::AutoSaveCheats)));
+        _settingsMenu.Append(new MenuEntryTools("Auto-save enabled cheats", [] { Preferences::Toggle(Preferences::AutoSaveCheats); }, true, Preferences::IsEnabled(Preferences::AutoSaveCheats)));
         _settingsMenu.Append(new MenuEntryTools("Auto-save favorites", [] { Preferences::Toggle(Preferences::AutoSaveFavorites); }, true, Preferences::IsEnabled(Preferences::AutoSaveFavorites)));
-        _settingsMenu.Append(new MenuEntryTools("Auto-load enabled cheats at starts", [] { Preferences::Toggle(Preferences::AutoLoadCheats); }, true, Preferences::IsEnabled(Preferences::AutoLoadCheats)));
-        _settingsMenu.Append(new MenuEntryTools("Auto-load favorites at starts", [] { Preferences::Toggle(Preferences::AutoLoadFavorites); }, true, Preferences::IsEnabled(Preferences::AutoSaveFavorites)));
+        _settingsMenu.Append(new MenuEntryTools("Auto-load enabled cheats at start-up", [] { Preferences::Toggle(Preferences::AutoLoadCheats); }, true, Preferences::IsEnabled(Preferences::AutoLoadCheats)));
+        _settingsMenu.Append(new MenuEntryTools("Auto-load favorites at start-up", [] { Preferences::Toggle(Preferences::AutoLoadFavorites); }, true, Preferences::IsEnabled(Preferences::AutoSaveFavorites)));
         _settingsMenu.Append(new MenuEntryTools("Load enabled cheats now", [] { Preferences::LoadSavedEnabledCheats(); }, nullptr));
         _settingsMenu.Append(new MenuEntryTools("Load favorites now", [] { Preferences::LoadSavedFavorites(); }, nullptr));
-*/
+
 
         // Get strings x position
         g_textXpos[0] = (320 - Renderer::LinuxFontSize(g_ctrpfText)) / 2;
