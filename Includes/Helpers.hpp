e7e8f0bca81a36958b0cfad045716f3e19cb7cb9
IcefirePhoenix <109490792+IcefirePhoenix@users.noreply.github.com>
2024-01-08 14:17:52 -0800

    Add Level helper class

--- Includes/Helpers.hpp
+++ Includes/Helpers.hpp
@@ -4,6 +4,7 @@
 #include "Helpers/Address.hpp"
 #include "Helpers/HoldKey.hpp"
 #include "Helpers/KeySequence.hpp"
+#include "Helpers/Level.hpp"
 #include "Helpers/MenuEntryHelpers.hpp"
 #include "Helpers/OSDManager.hpp"
 #include "Helpers/QuickMenu.hpp"
---
+++ Includes/Helpers/Level.hpp
@@ -0,0 +1,27 @@
+#ifndef LEVEL_HPP
+#define LEVEL_HPP
+
+#include "Helpers.hpp"
+#include "AddressList.hpp"
+#include <3ds.h>
+#include "CTRPluginFramework.hpp"
+
+namespace CTRPluginFramework {
+	class Level {
+	public:
+		Level(u8 ID, std::string externalName, std::string internalName, bool dummyStatus);
+
+		static const Level levelList[];
+
+		static u8 getIDFromName(const std::string& name);
+		static u8 getCurrLevel(void);
+		static bool isInDrablands(void);
+
+	private:
+		u8				_levelID;
+		std::string		_extName;
+		std::string		_intName;
+		bool			_isDummy;
+	};
+}
+#endif
