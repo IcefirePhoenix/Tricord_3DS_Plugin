6db19d4b9711ef4bd92e33c5e7f8e5fa18972dd9
IcefirePhoenix <109490792+IcefirePhoenix@users.noreply.github.com>
2024-01-09 10:54:30 -0800

    Refactor Rendering codes

--- Sources/Rendering.cpp
+++ Sources/Rendering.cpp
@@ -1,49 +1,54 @@
 #include "cheats.hpp"
 #include "AddressList.hpp"
 #include "Helpers/Address.hpp"
+#include "Helpers/GeneralHelpers.hpp"
+
 #include "3ds.h"
 
 namespace CTRPluginFramework
 {
-    void hideHUD(MenuEntry* entry) {
+    bool isScrollTextDisabled = false;
+
+    void triggerHideHUD(MenuEntry* entry) {
+        hideHUD();
+    }
+
+    void hideHUD(void) {
         // TODO: add doppel location indicators
+        u32 HUDPointerCheck;
+        Process::Read32(AddressList::HUDPointer.addr, HUDPointerCheck);
 
-        // prep check  -- if HUD pointer is null
-        u32 pointerCheck;
-        Process::Read32(AddressList::HUDPointer.addr, pointerCheck);
-
-        // define offsets for easy reference
         u32 mainHUDoffset = 0x00000018;
         u32 ActionButtonBGOffset = 0x000075D0;
-        u32 ActionBUttonTextOffset = 0x000078B0;
+        u32 ActionButtonTextOffset = 0x000078B0;
 
-        if (pointerCheck != 0x00000000) {
-            Process::Write32((pointerCheck + mainHUDoffset), 0x7F000004);
-            Process::Write32((pointerCheck + ActionButtonBGOffset), 0x00000010);
-            Process::Write32((pointerCheck + ActionBUttonTextOffset), 0x00FFFF00);
+        if (!GeneralHelpers::isNullPointer(HUDPointerCheck)) {
+            Process::Write32((HUDPointerCheck + mainHUDoffset), 0x7F000004);
+            Process::Write32((HUDPointerCheck + ActionButtonBGOffset), 0x00000010);
+            Process::Write32((HUDPointerCheck + ActionButtonTextOffset), 0x00FFFF00);
         }
     }
 
     // does this reset itself? 
     void disableFog(MenuEntry* entry) {
+        // eventually it would be nice to have proper labels for these addresses + values
+        // once enough info is known
+         
         Process::Write32((AddressList::FogA.addr), 0xFF700FFF);
         Process::Write32((AddressList::FogB.addr), 0x453B8000);
         Process::Write32((AddressList::FogC.addr), 0x459C4000);
     }
-
-    bool isScrollTextDisabled = false;
     
-    void disableScrollingText(MenuEntry* entry) { // this should be a toggle
+    void disableScrollingText(MenuEntry* entry) {
         isScrollTextDisabled = !isScrollTextDisabled;
 
         if (isScrollTextDisabled) {
             Process::Write32(AddressList::ScrollingTextOpacity.addr, 0x00000000);
-            MessageBox(Color::Gainsboro << "Success", "Scrolling Text has been disabled.")();
-
+            entry->SetName("Enable top screen scrolling text");
         }
         else {
             Process::Write32(AddressList::ScrollingTextOpacity.addr, 0x3F800000);
-            MessageBox(Color::Gainsboro << "Success", "Scrolling Text has been enabled.")();
+            entry->SetName("Disable top screen scrolling text");
         }
     }
 }
