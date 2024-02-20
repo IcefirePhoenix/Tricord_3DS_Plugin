9e968d49add51a4c744ff6728d9b273bd907737b
Supergleem <94009741+Supergleem@users.noreply.github.com>
2024-02-20 13:05:46 -0500

    Added remaining EU Addresses

--- Sources/Main/AddressList.cpp
+++ Sources/Main/AddressList.cpp
@@ -5,10 +5,10 @@ namespace CTRPluginFramework {
 
 	void AddressList::InitAddresses(void) {
 		/* Camera */ 
-		DynamicCameraCheck 			= Address(0x00143810, 0x00000000, 0x00000000, 0x00000000);
-		GameplayCameraCheck 		= Address(0x00143A7C, 0x00000000, 0x00000000, 0x00000000);
-		GameplayCameraInit 			= Address(0x00143A80, 0x00000000, 0x00000000, 0x00000000);
-		RetGameplayCameraInit 		= Address(0x00143B80, 0x00000000, 0x00000000, 0x00000000);
+		DynamicCameraCheck 			= Address(0x00143810, 0x0014382C, 0x00000000, 0x00000000);
+		GameplayCameraCheck 		= Address(0x00143A7C, 0x00143A98, 0x00000000, 0x00000000);
+		GameplayCameraInit 			= Address(0x00143A80, 0x00143A9C, 0x00000000, 0x00000000);
+		RetGameplayCameraInit 		= Address(0x00143B80, 0x00143B9C, 0x00000000, 0x00000000);
 
 		CameraLinkFocus 			= Address(0x32C815D0, 0x32C81650, 0x00000000, 0x00000000);
 		CameraMode 					= Address(0x32C814BC, 0x32C8153C, 0x00000000, 0x00000000);
@@ -18,21 +18,21 @@ namespace CTRPluginFramework {
 		CameraRotationX 			= Address(0x32C81520, 0x32C815A0, 0x00000000, 0x00000000);
 		CameraRotationZ 			= Address(0x32C81524, 0x32C815A4, 0x00000000, 0x00000000);
 		OrthographicZoom 			= Address(0x32C81528, 0x32C815A8, 0x00000000, 0x00000000);
-		PerspectiveZoom 			= Address(0x005F529C, 0x00000000, 0x00000000, 0x00000000);
+		PerspectiveZoom 			= Address(0x005F529C, 0x005F56CC, 0x00000000, 0x00000000);
 
 		/* Chaos */ 
 		HeartsTakeHealth 			= Address(0x0015CA04, 0x0015CA20, 0x00000000, 0x00000000);
 
 		/* Costume */ 
-		CostumeAttrA 				= Address(0x32849510, 0x00000000, 0x00000000, 0x00000000);
-		CostumeAttrB 				= Address(0x32849512, 0x00000000, 0x00000000, 0x00000000);
-		CostumeAttrC 				= Address(0x32849513, 0x00000000, 0x00000000, 0x00000000);
-		CostumeAttrD				= Address(0x32849514, 0x00000000, 0x00000000, 0x00000000);
+		CostumeAttrA 				= Address(0x32849510, 0x32849590, 0x00000000, 0x00000000);
+		CostumeAttrB 				= Address(0x32849512, 0x32849592, 0x00000000, 0x00000000);
+		CostumeAttrC 				= Address(0x32849513, 0x32849593, 0x00000000, 0x00000000);
+		CostumeAttrD				= Address(0x32849514, 0x32849594, 0x00000000, 0x00000000);
 		CurrCostume 				= Address(0x32849564, 0x328495E4, 0x00000000, 0x00000000);
 		CostumeCatalogPointer 		= Address(0x32C828E8, 0x32C82968, 0x00000000, 0x00000000);
-		PlayerStatus 				= Address(0x32849511, 0x00000000, 0x00000000, 0x00000000);
+		PlayerStatus 				= Address(0x32849511, 0x32849591, 0x00000000, 0x00000000);
 		TingleBalloons 				= Address(0x32849680, 0x32849700, 0x00000000, 0x00000000);
-		UnusedCostumeDataPointers 	= Address(0x0075C390, 0x00000000, 0x00000000, 0x00000000);
+		UnusedCostumeDataPointers 	= Address(0x0075C390, 0x0075C398, 0x00000000, 0x00000000);
 
 		/* Console */ 
 
@@ -52,25 +52,25 @@ namespace CTRPluginFramework {
 		/* Game Data */ 
 		CurrLevelID 				= Address(0x3280EDC4, 0x3280EE44, 0x00000000, 0x00000000);
 		CurrStageID 				= Address(0x3280EDC8, 0x3280EE48, 0x00000000, 0x00000000);
-		CurrSpawnID 				= Address(0x3280EDCA, 0x3280EE4C, 0x00000000, 0x00000000);
+		CurrSpawnID 				= Address(0x3280EDCA, 0x3280EE4A, 0x00000000, 0x00000000);
 		DoppelsEnabled 				= Address(0x32D81E46, 0x32D81EC6, 0x00000000, 0x00000000);
 		DrablandsRupeeCount 		= Address(0x32C8178C, 0x32C8180C, 0x00000000, 0x00000000);
-		EnemyDataPointer			= Address(0x32989188, 0x00000000, 0x00000000, 0x00000000);
+		EnemyDataPointer			= Address(0x32989188, 0x32989208, 0x00000000, 0x00000000);
 		HealthCurrent 				= Address(0x32C824F4, 0x32C82574, 0x00000000, 0x00000000);
 		HealthMax 					= Address(0x32C824F5, 0x32C82575, 0x00000000, 0x00000000);
-		EnergyCurrent				= Address(0x328496C4, 0x00000000, 0x00000000, 0x00000000);
-		EnergyMax					= Address(0x00192644, 0x00000000, 0x00000000, 0x00000000);
-		EnergyMaxPitRecovery		= Address(0x001925F8, 0x00000000, 0x00000000, 0x00000000);
-		EnergyMaxMultiplicand		= Address(0x00192684, 0x00000000, 0x00000000, 0x00000000);
-		FairiesCurrent				= Address(0x32D81F14, 0x00000000, 0x00000000, 0x00000000);
-		TimeLeft					= Address(0x32D81F18, 0x00000000, 0x00000000, 0x00000000);
-		CollisionCurrent			= Address(0x328416A4, 0x00000000, 0x00000000, 0x00000000);
-		SpeedX						= Address(0x32841650, 0x00000000, 0x00000000, 0x00000000);
-		SpeedY						= Address(0x32841654, 0x00000000, 0x00000000, 0x00000000);
-		SpeedZ						= Address(0x32841658, 0x00000000, 0x00000000, 0x00000000);
-		TotemCarrying				= Address(0x32849520, 0x00000000, 0x00000000, 0x00000000);
-		TotemCarriedBy				= Address(0x32849524, 0x00000000, 0x00000000, 0x00000000);
-		LoadingStatus 				= Address(0x319FE1D0, 0x00000000, 0x00000000, 0x00000000);
+		EnergyCurrent				= Address(0x328496C4, 0x32849744, 0x00000000, 0x00000000);
+		EnergyMax					= Address(0x00192644, 0x001926D4, 0x00000000, 0x00000000);
+		EnergyMaxPitRecovery		= Address(0x001925F8, 0x00192588, 0x00000000, 0x00000000);
+		EnergyMaxMultiplicand		= Address(0x00192684, 0x00192714, 0x00000000, 0x00000000);
+		FairiesCurrent				= Address(0x32D81F14, 0x32D81F94, 0x00000000, 0x00000000);
+		TimeLeft					= Address(0x32D81F18, 0x32D81F98, 0x00000000, 0x00000000);
+		CollisionCurrent			= Address(0x328416A4, 0x32841724, 0x00000000, 0x00000000);
+		SpeedX						= Address(0x32841650, 0x328416D0, 0x00000000, 0x00000000);
+		SpeedY						= Address(0x32841654, 0x328416D4, 0x00000000, 0x00000000);
+		SpeedZ						= Address(0x32841658, 0x328416D8, 0x00000000, 0x00000000);
+		TotemCarrying				= Address(0x32849520, 0x328495A0, 0x00000000, 0x00000000);
+		TotemCarriedBy				= Address(0x32849524, 0x328495A4, 0x00000000, 0x00000000);
+		LoadingStatus 				= Address(0x319FE1D0, 0x319FE228, 0x00000000, 0x00000000);
 		PauseStatus 				= Address(0x3280ED8C, 0x3280EE0C, 0x00000000, 0x00000000);
 		PreviousLevelID				= Address(0x3280EDBC, 0x3280EE3C, 0x00000000, 0x00000000);
 		PreviousStageID				= Address(0x3280EDC0, 0x3280EE40, 0x00000000, 0x00000000);
@@ -83,16 +83,16 @@ namespace CTRPluginFramework {
 		GustStrafe 					= Address(0x003C85A8, 0x003C8950, 0x00000000, 0x00000000);
 		HammerStrafe 				= Address(0x003C03E0, 0x003C0788, 0x00000000, 0x00000000);
 		ShadowLinkItemPointer 		= Address(0x32D81DEC, 0x32D81E6C, 0x00000000, 0x00000000);
-		RoboNiceAtk 				= Address(0x00156AB4, 0x00000000, 0x00000000, 0x00000000);
-		RoboNiceSFX 				= Address(0x003C2030, 0x00000000, 0x00000000, 0x00000000);
-		RoboNiceSpeed 				= Address(0x00156898, 0x00000000, 0x00000000, 0x00000000);
+		RoboNiceAtk 				= Address(0x00156AB4, 0x00156AD0, 0x00000000, 0x00000000);
+		RoboNiceSFX 				= Address(0x003C2030, 0x003C23D8, 0x00000000, 0x00000000);
+		RoboNiceSpeed 				= Address(0x00156898, 0x001568B4, 0x00000000, 0x00000000);
 		RoboStrafe 					= Address(0x003C214C, 0x003C24F4, 0x00000000, 0x00000000);
 		WaterRodStrafe 				= Address(0x003C59C0, 0x003C5D68, 0x00000000, 0x00000000);
 
 		/* Miscellaneous */ 
 		CameraOnX 					= Address(0x32D92920, 0x32D929A0, 0x00000000, 0x00000000);
 		CameraShutter 				= Address(0x32D92E9C, 0x32D92F1C, 0x00000000, 0x00000000);		
-		CheckPhotoExist 			= Address(0x31B2FE3D, 0x00000000, 0x00000000, 0x00000000);
+		CheckPhotoExist 			= Address(0x31B2FE3D, 0x31B2FEBD, 0x00000000, 0x00000000);
 		DisplayTopPhoto 			= Address(0x31B2FE21, 0x31B2FEA1, 0x00000000, 0x00000000);
 		SeeChestContents 			= Address(0x0022FFB8, 0x00230040, 0x00000000, 0x00000000);
 		SwordBeamCD 				= Address(0x328495A0, 0x32849620, 0x00000000, 0x00000000);
@@ -109,7 +109,7 @@ namespace CTRPluginFramework {
 		IsInvincible 				= Address(0x32849544, 0x328495C4, 0x00000000, 0x00000000);
 		IsJinxed 					= Address(0x328495AC, 0x3284962C, 0x00000000, 0x00000000);
 		IsSpawned 					= Address(0x32841410, 0x32841490, 0x00000000, 0x00000000);
-		IsSwapping 					= Address(0x31A04610, 0x00000000, 0x00000000, 0x00000000);
+		IsSwapping 					= Address(0x31A04610, 0x031A04668, 0x00000000, 0x00000000);
 		IsWaterStorage				= Address(0x32849231, 0x328492B1, 0x00000000, 0x00000000);
 		IsVisible 					= Address(0x32849550, 0x328495D0, 0x00000000, 0x00000000);
 		LinkVoice 					= Address(0x3284A260, 0x3284A2E0, 0x00000000, 0x00000000);
@@ -130,46 +130,46 @@ namespace CTRPluginFramework {
 		SwordType 					= Address(0x328419EC, 0x32841A6C, 0x00000000, 0x00000000);
 
 		/* Rendering */ 
-		ArrowIndicatorColorBot 		= Address(0x32D91B24, 0x00000000, 0x00000000, 0x00000000);
-		ArrowIndicatorColorMid 		= Address(0x32D91A04, 0x00000000, 0x00000000, 0x00000000);
-		ArrowIndicatorColorTop 		= Address(0x32D918E4, 0x00000000, 0x00000000, 0x00000000);
-		HUDPointer 					= Address(0x32C8289C, 0x00000000, 0x00000000, 0x00000000);
-		FogA 						= Address(0x31B31BE4, 0x00000000, 0x00000000, 0x00000000);
-		FogB 						= Address(0x31B31BDC, 0x00000000, 0x00000000, 0x00000000);
-		FogC 						= Address(0x31B31BE0, 0x00000000, 0x00000000, 0x00000000);
-		ScrollingTextOpacity 		= Address(0x00151C40, 0x00000000, 0x00000000, 0x00000000);
-		OffScreenLocation 			= Address(0x32D83094, 0x00000000, 0x00000000, 0x00000000);
-		TextureName					= Address(0x3033D7C3, 0x00000000, 0x00000000, 0x00000000);
-		LoadingScreenSPNoChal		= Address(0x002B1410, 0x00000000, 0x00000000, 0x00000000);
-		LoadingScreenSPIcons		= Address(0x00390C08, 0x00000000, 0x00000000, 0x00000000);
+		ArrowIndicatorColorBot 		= Address(0x32D91B24, 0x32D91BA4, 0x00000000, 0x00000000);
+		ArrowIndicatorColorMid 		= Address(0x32D91A04, 0x32D91A84, 0x00000000, 0x00000000);
+		ArrowIndicatorColorTop 		= Address(0x32D918E4, 0x32D91964, 0x00000000, 0x00000000);
+		HUDPointer 					= Address(0x32C8289C, 0x32C8291C, 0x00000000, 0x00000000);
+		FogA 						= Address(0x31B31BE4, 0x31B31C64, 0x00000000, 0x00000000);
+		FogB 						= Address(0x31B31BDC, 0x31B31C5C, 0x00000000, 0x00000000);
+		FogC 						= Address(0x31B31BE0, 0x31B31C60, 0x00000000, 0x00000000);
+		ScrollingTextOpacity 		= Address(0x00151C40, 0x00151C5C, 0x00000000, 0x00000000);
+		OffScreenLocation 			= Address(0x32D83094, 0x32D83114, 0x00000000, 0x00000000);
+		TextureName					= Address(0x3033D7C3, 0x30363E43, 0x00000000, 0x00000000);
+		LoadingScreenSPNoChal		= Address(0x002B1410, 0x002B17B4, 0x00000000, 0x00000000);
+		LoadingScreenSPIcons		= Address(0x00390C08, 0x00390FB0, 0x00000000, 0x00000000);
 
 		/* Savefile */ 
-		NormalNCCompletion 			= Address(0x31A060EC, 0x00000000, 0x00000000, 0x00000000);
-		NormalC1Completion 			= Address(0x31A060F4, 0x00000000, 0x00000000, 0x00000000);
-		NormalC2Completion 			= Address(0x31A060FC, 0x00000000, 0x00000000, 0x00000000);
-		NormalC3Completion 			= Address(0x31A06104, 0x00000000, 0x00000000, 0x00000000);
-		SoloNCCompletion 			= Address(0x31A0616C, 0x00000000, 0x00000000, 0x00000000);
-		SoloC1Completion 			= Address(0x31A06174, 0x00000000, 0x00000000, 0x00000000);
-		SoloC2Completion 			= Address(0x31A0617C, 0x00000000, 0x00000000, 0x00000000);
-		SoloC3Completion 			= Address(0x31A06184, 0x00000000, 0x00000000, 0x00000000);
-		MultiNCCompletion 			= Address(0x31A0612C, 0x00000000, 0x00000000, 0x00000000);
-		MultiC1Completion 			= Address(0x31A06134, 0x00000000, 0x00000000, 0x00000000);
-		MultiC2Completion 			= Address(0x31A0613C, 0x00000000, 0x00000000, 0x00000000);
-		MultiC3Completion 			= Address(0x31A06144, 0x00000000, 0x00000000, 0x00000000);
+		NormalNCCompletion 			= Address(0x31A060EC, 0x31A06144, 0x00000000, 0x00000000);
+		NormalC1Completion 			= Address(0x31A060F4, 0x31A0614C, 0x00000000, 0x00000000);
+		NormalC2Completion 			= Address(0x31A060FC, 0x31A06154, 0x00000000, 0x00000000);
+		NormalC3Completion 			= Address(0x31A06104, 0x31A0615C, 0x00000000, 0x00000000);
+		SoloNCCompletion 			= Address(0x31A0616C, 0x31A061C4, 0x00000000, 0x00000000);
+		SoloC1Completion 			= Address(0x31A06174, 0x31A061CC, 0x00000000, 0x00000000);
+		SoloC2Completion 			= Address(0x31A0617C, 0x31A061D4, 0x00000000, 0x00000000);
+		SoloC3Completion 			= Address(0x31A06184, 0x31A061DC, 0x00000000, 0x00000000);
+		MultiNCCompletion 			= Address(0x31A0612C, 0x31A06184, 0x00000000, 0x00000000);
+		MultiC1Completion 			= Address(0x31A06134, 0x31A0618C, 0x00000000, 0x00000000);
+		MultiC2Completion 			= Address(0x31A0613C, 0x31A06194, 0x00000000, 0x00000000);
+		MultiC3Completion 			= Address(0x31A06144, 0x31A0619C, 0x00000000, 0x00000000);
 
-		CostumeObtainStatus 		= Address(0x32C8256A, 0x00000000, 0x00000000, 0x00000000);
-		ColiseumWinCount 			= Address(0x31A062AC, 0x00000000, 0x00000000, 0x00000000);
-		EditMerchantStock 			= Address(0x31A077A4, 0x00000000, 0x00000000, 0x00000000);
-		HeroPointCount 				= Address(0x31A0611C, 0x00000000, 0x00000000, 0x00000000);
-		MainVoice 					= Address(0x31A06118, 0x00000000, 0x00000000, 0x00000000);
-		ResetMerchantStock 			= Address(0x31A077AE, 0x00000000, 0x00000000, 0x00000000);
+		CostumeObtainStatus 		= Address(0x32C8256A, 0x32C825EA, 0x00000000, 0x00000000);
+		ColiseumWinCount 			= Address(0x31A062AC, 0x31A06304, 0x00000000, 0x00000000);
+		EditMerchantStock 			= Address(0x31A077A4, 0x31A077FC, 0x00000000, 0x00000000);
+		HeroPointCount 				= Address(0x31A0611C, 0x31A06174, 0x00000000, 0x00000000);
+		MainVoice 					= Address(0x31A06118, 0x31A06170, 0x00000000, 0x00000000);
+		ResetMerchantStock 			= Address(0x31A077AE, 0x31A07806, 0x00000000, 0x00000000);
 
 		// SFX and BGM
-		BGMType 					= Address(0x005FD3C0, 0x00000000, 0x00000000, 0x00000000);
-		BGMVolume 					= Address(0x00236444, 0x00000000, 0x00000000, 0x00000000);
-		LobbyBallPointer			= Address(0x32DCAA60, 0x00000000, 0x00000000, 0x00000000);
-		LinkVoiceSpeed 				= Address(0x3284A25C, 0x00000000, 0x00000000, 0x00000000);
-		MasterVolume 				= Address(0x0050BC74, 0x00000000, 0x00000000, 0x00000000);
+		BGMType 					= Address(0x005FD3C0, 0x005FD7F0, 0x00000000, 0x00000000);
+		BGMVolume 					= Address(0x00236444, 0x002364D4, 0x00000000, 0x00000000);
+		LobbyBallPointer			= Address(0x32DCAA60, 0x32DCAAE0, 0x00000000, 0x00000000);
+		LinkVoiceSpeed 				= Address(0x3284A25C, 0x3284A2DC, 0x00000000, 0x00000000);
+		MasterVolume 				= Address(0x0050BC74, 0x0050BDF0, 0x00000000, 0x00000000);
 	}
 
 	// Camera	
