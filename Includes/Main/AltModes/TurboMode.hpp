2e19ea095187f36f041c8d7ade15fd767d359562
Peanut <rafflybofd@gmail.com>
2024-02-24 19:46:11 -0500

    Turbo Mode files

---
+++ Includes/Main/AltModes/TurboMode.hpp
@@ -0,0 +1,15 @@
+#ifndef TURBOMODE_HPP
+#define TURBOMODE_HPP
+
+#include "CTRPluginFramework.hpp"
+#include "Helpers.hpp"
+#include "Cheats.hpp"
+#include "Unicode.h"
+
+namespace CTRPluginFramework {
+    class TurboMode {
+    public:
+        static void enableTurboMode(MenuEntry* entry);
+    };
+}
+#endif
