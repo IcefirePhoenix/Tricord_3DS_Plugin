eaa88ae528f894398e7fb5d38e319d781a5cdfac
IcefirePhoenix <109490792+IcefirePhoenix@users.noreply.github.com>
2024-03-29 22:49:18 -0700

    Only offset addresses in LINEAR mem range

--- Sources/Helpers/DataHelpers/Address.cpp
+++ Sources/Helpers/DataHelpers/Address.cpp
@@ -5,45 +5,45 @@
 namespace CTRPluginFramework {
 	u32 _memOffset = 0x0;
 
-	Address::Address(u32 usa, u32 eur, u32 jpn, u32 kor) 
+	Address::Address(u32 usa, u32 eur, u32 jpn, u32 kor)
 	{
-		switch (Process::GetTitleID()) 
+		switch (Process::GetTitleID())
 		{
 			case TID_USA:
-				addr = usa; 
+				addr = usa;
 				break;
 
 			case TID_EUR:
-				addr = eur; 
+				addr = eur;
 				break;
 
 			case TID_JPN:
-				addr = jpn; 
+				addr = jpn;
 				break;
 
 			case TID_KOR:
-				addr = kor; 
+				addr = kor;
 				break;
 
-			default: 
+			default:
 				addr = 0;
-				OSD::Notify("The current title is not a supported, retail version of TFH!", Color::Red);
+				OSD::Notify("The current title is not a supported retail version of TFH!", Color::Red);
 				OSD::Notify("Supported TFH regions include NA, EU/AUS, JP, and KOR.", Color::Red);
 				OSD::Notify("Addresses cannot be initialized.", Color::Red);
 				break;
 		}
 
-		addr -= _memOffset;
+		if (addr >= 0x14000000) addr -= _memOffset;
 	}
 
-	Address::Address(u32 address) 
+	Address::Address(u32 address)
 	{
 		addr = address;
 	};
 
 	// this might need to be removed
 	// null addresses aren't needed for Tricord
-	Address::Address(void) 
+	Address::Address(void)
 	{
 		addr = 0;
 	};
@@ -54,7 +54,7 @@ namespace CTRPluginFramework {
 		u32 temp;
 
 		_memOffset = Process::CheckRegion(startAddress, temp) ? 0x0 : 0x1C000000;
-			
+
 		if (_memOffset == 0x1C000000)
 			OSD::Notify("Alternate address set loaded.");
 	}
