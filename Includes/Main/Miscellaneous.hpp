97fb53cf78951d7fcbfee24e108bdba55a9e77c6
IcefirePhoenix <109490792+IcefirePhoenix@users.noreply.github.com>
2024-12-11 00:54:00 -0800

    Fix button spammer and add support for custom timer intervals

--- Includes/Main/Miscellaneous.hpp
+++ Includes/Main/Miscellaneous.hpp
@@ -17,10 +17,34 @@ namespace CTRPluginFramework {
     void setCameraEdits(bool useCamera);
     bool getCameraEdits(void);
 
+    const u16 BUTTON_A = 1 << 0;
+    const u16 BUTTON_B = 1 << 1;
+    const u16 BUTTON_X = 1 << 2;
+    const u16 BUTTON_Y = 1 << 3;
+    const u16 BUTTON_L = 1 << 4;
+    const u16 BUTTON_R = 1 << 5;
+    const u16 BUTTON_STARTSEL = 1 << 6;
+    const u16 BUTTON_TOUCH = 1 << 7;
+
+    const Key keys[] =
+    {
+        Key::A,
+        Key::B,
+        Key::X,
+        Key::Y,
+        Key::L,
+        Key::R,
+        Key::Start,
+        Key::Select,
+        Key::Touchpad
+    };
+
     class Miscellaneous {
     public:
         static void buttonSpammer(MenuEntry* entry);
-        static void managePhotoDisp(MenuEntry* entry);
+        static void selSpamInterval(MenuEntry *entry);
+        static void selectSpamKeys(MenuEntry *entry);
+        static void managePhotoDisp(MenuEntry *entry);
         static void manageInstantText(MenuEntry* entry);
         static void selectLinkBeam(MenuEntry* entry);
         static void setBeamCooldown(MenuEntry* entry);
